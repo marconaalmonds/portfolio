ca69d8f9a47a6e9518afbb455d887de297b33b40
marconaalmonds <kevinliebetrau7@gmail.com>
2019-11-19 17:20:35 +0000

    my first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Kevin Liebetrau CSP Period 2]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Liebetrau_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Liebetrau_Projects.html">Projects.html</a></li></br>
+      <li><a href="Liebetrau_Resume.html">Resume.html</a></li></br>
+      <li><a href="Liebetrau_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
